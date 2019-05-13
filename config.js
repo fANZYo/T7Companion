ff6f35c382a23da62b9b8f6a6161cafe1c1e567c
fANZYo <www.iehl@gmail.com>
2019-05-13 17:46:12 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	database: {
+		url: 'mongodb://localhost:27017',
+		collection: 'characters',
+	}
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const config = require('./config');
+
+const app = express();
+
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.originalUrl}`);
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.send('T7api running...');
+});
+
+// eslint-disable-next-line consistent-return
+MongoClient.connect(config.database.url, { useNewUrlParser: true }, (err, client) => {
+	if (err) {
+		console.log('Error while connecting to mongo database');
+		return 1;
+	}
+
+	app.locals.db = client.db('t7api');
+
+	const port = process.env.PORT || 5000;
+	app.listen(port, () => {
+		console.log(`Server running on port: ${port}`);
+	});
+});
