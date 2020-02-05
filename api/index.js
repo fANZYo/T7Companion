d7c0377c54e07105941b2898674917fd502343c5
fANZYo <www.iehl@gmail.com>
2020-02-05 22:29:18 +0000

    feat: add node cluster

--- api/index.js
+++ api/index.js
@@ -1,47 +1,11 @@
-const express = require('express');
-const { Pool } = require('pg');
-const redis = require('redis');
+const cluster = require('cluster');
+const cpus = require('os').cpus();
 
-const pool = new Pool({
-	host: process.env.PGHOST,
-	port: process.env.PGPORT,
-	user: process.env.PGUSER,
-	database: process.env.PGDATABASE,
-});
-
-const boolParser = require('express-query-boolean');
-const {
-	lowerQuery,
-} = require('./controllers/middleware');
-
-const {
-	rootRoute,
-	characterRouter,
-} = require('./routes');
-
-const app = express();
-
-// Middleware
-app.use(boolParser());
-app.use(lowerQuery);
-
-// Routes
-app.use('/', rootRoute);
-app.use('/character', characterRouter);
-
-const redisUrl = process.env.REDISCLOUD_URI;
-// eslint-disable-next-line consistent-return
-pool.connect((err, client) => {
-	if (err) {
-		console.log('Error while connecting to postgres database:', process.env.PGDATABASE);
-		return 1;
-	}
-
-	app.locals.pg = client;
-	app.locals.redis = redis.createClient(`redis://${redisUrl}`, { no_ready_check: true });
-
-	const port = process.env.PORT || 5000;
-	app.listen(port, () => {
-		console.log(`Server running on port: ${port}`);
+if (cluster.isMaster) {
+	cpus.forEach(() => {
+		cluster.fork();
 	});
-});
+} else {
+	console.log(`Worker ${process.pid} started`);
+	require('./server.js'); // eslint-disable-line global-require
+}
---
+++ api/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { Pool } = require('pg');
+const redis = require('redis');
+
+const pool = new Pool({
+	host: process.env.PGHOST,
+	port: process.env.PGPORT,
+	user: process.env.PGUSER,
+	database: process.env.PGDATABASE,
+});
+
+const boolParser = require('express-query-boolean');
+const {
+	lowerQuery,
+} = require('./controllers/middleware');
+
+const {
+	rootRoute,
+	characterRouter,
+} = require('./routes');
+
+const app = express();
+
+// Middleware
+app.use(boolParser());
+app.use(lowerQuery);
+
+// Routes
+app.use('/', rootRoute);
+app.use('/character', characterRouter);
+
+const redisUrl = process.env.REDISCLOUD_URI;
+// eslint-disable-next-line consistent-return
+pool.connect((err, client) => {
+	if (err) {
+		console.log('Error while connecting to postgres database:', process.env.PGDATABASE);
+		return 1;
+	}
+
+	app.locals.pg = client;
+	app.locals.redis = redis.createClient(`redis://${redisUrl}`, { no_ready_check: true });
+
+	const port = process.env.PORT || 5000;
+	app.listen(port, () => {
+		console.log(`Server running on port: ${port}`);
+	});
+});
